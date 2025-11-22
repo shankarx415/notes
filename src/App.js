ab4d41c6094c6599e2709d6a40f4459e2106145d
shankarx415 <shankarlaptop0506@gmail.com>
2025-11-22 21:24:48 +0530

    Fix routing and import for CNotes component

--- src/App.js
+++ src/App.js
@@ -2,23 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-// Import all Page Components
 import BeeNotes from './pages/BeeNotes';
 import EnglishNotes from './pages/EnglishNotes';
 import ItNotes from './pages/ItNotes';
-import CNotes from './pages/CNotes'; // <--- 1. Make sure this import is here
+import CNotes from './pages/CNotes';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/notes">
       <div className="App">
         <Routes>
-          {/* Existing Routes */}
-          <Route path="/" element={<BeeNotes />} />
+          {/* Default route for /notes/ */}
+          <Route index element={<BeeNotes />} />
+
           <Route path="/english" element={<EnglishNotes />} />
           <Route path="/it" element={<ItNotes />} />
-
-          {/* New Route - This is what fixes the error */}
           <Route path="/c-programming" element={<CNotes />} />
         </Routes>
       </div>
